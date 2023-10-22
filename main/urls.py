e5791fe0b3bc17b0ef6484d549382975e7f238a7
Haru18Mori <119049595+Haru18Mori@users.noreply.github.com>
2023-10-23 00:09:18 +0900

    クラブのpk取得苦戦中

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.IndexView.as_view(), name="home"),
     path("club",views.ClubView.as_view(),name="club"),
+    path("club__detail/<int:pk>", views.ClubDetailView.as_view(), name="club__detail"),
     path("access",views.AccessView.as_view(),name="access"),
     path("music",views.MusicView.as_view(),name="music"),
     path("map",views.MapView.as_view(),name="map"),
